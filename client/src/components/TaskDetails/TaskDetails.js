db6d6a9fe7035cc000e6160eeec07b41c0212bd7
Nur A Alam <nuraalam376@gmail.com>
2021-07-06 22:20:12 +0600

    started implementing draggable feature

--- client/src/components/TaskDetails/TaskDetails.js
+++ client/src/components/TaskDetails/TaskDetails.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./TaskDetails.css";
 
-function TaskDetails({task}) {
+function TaskDetails({task, onDragStart, onDragOver, onDrop}) {
     const {name} = task;
+
     return (
-        <p className = "taskName">
+        <p 
+            className = "taskName" 
+            draggable 
+            onDragStart = {e => onDragStart(e,name)} 
+            onDragOver  = {(e) => onDragOver(e)}
+            onDrop = {(e) => onDrop(e, name)}
+            >
             {name}
         </p>
     )
--- client/src/components/Tasks/Tasks.js
+++ client/src/components/Tasks/Tasks.js
@@ -3,10 +3,35 @@ import TaskDetails from '../TaskDetails/TaskDetails';
 
 function Tasks({taskState}) {
     const {tasks,loading, error} = taskState;
+    
+    const onDragStart = (e,taskName) => {
+        e.dataTransfer.setData("id",taskName);
+    };
+    
+    const onDragOver = (e) => {
+        e.preventDefault();
+        console.log("drag Over",e.dataTransfer.getData("id"));
+    };
+
+    const onDrop = (e, taskName) => {
+        e.stopPropagation();
+        e.preventDefault();
+        console.log("drop Over", taskName);
+    };
+    
         return (
         <div>
             {loading && <h1>Loading ....</h1>}
-            {tasks.length ? tasks.map(task => <TaskDetails key = {task._id} task={task}/> ): <p>No Tasks</p>}
+            {tasks.length ? tasks.map(task => 
+                    <TaskDetails 
+                        key = {task._id} 
+                        task={task} 
+                        onDragStart = {onDragStart} 
+                        onDragOver = {onDragOver}
+                        onDrop = {onDrop}
+                    /> 
+                )
+                    : <p>No Tasks</p>}
             {error && <p>{error}</p>}
         </div>
     )
